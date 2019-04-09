6ecdda2cfb8c47963f67523a0c40450fd523d9f3
liuyj <975399405@qq.com>
2019-04-09 14:11:15 +0800

    增加招商零星无积分商户查询页

--- static/js/common.js
+++ static/js/common.js
@@ -3,5 +3,26 @@
 //系统配置
 var sysconfig = {
 	url:'http://47.74.183.195:8088'
+    //url:'http://localhost:8088'
 }
 
+/**
+ * 右下角显示信息，淡蓝色方块
+ * @param msg
+ */
+function showMsg(msg, type, offset) {
+    var type = type == null ? "info" : type;
+    var offset = offset == null ? {from: 'bottom', amount: 20} : offset;
+    $.bootstrapGrowl(msg,
+        {
+            ele: 'body', // which element to append to
+            type: type, // (null, 'info', 'danger', 'success')，三种显示样式，INFO（天蓝色），danger（淡红色），success（淡绿色）
+            offset: offset, // 'top', or 'bottom'
+            align: 'right', // ('left', 'right', or 'center')
+            width: 'auto', // (integer, or 'auto')
+            delay: 2000,	//延迟4s，即4s后消失
+            allow_dismiss: true,
+            stackup_spacing: 10 // spacing between consecutively stacked growls.
+        }
+    );
+}
